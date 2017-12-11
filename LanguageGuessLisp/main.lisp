672bb8293c37eb92f1d84ebead90e20e60dc1d24
holgus103 <kubasuchan@hotmail.com>
2017-12-11 23:29:55 +0100

    Project draft

--- LanguageGuessLisp/main.lisp
+++ LanguageGuessLisp/main.lisp
@@ -1,37 +1,65 @@
-(defstruct language
-	name
-	logograms
-	left-to-right
-	abjad
-	cyrulic
-	accents
+; variables for storing information whether a question has been asked already
+(defvar logograms)
+(defvar left-to-right)
+(defvar abjad)
+(defvar cyrulic)
+(defvar accents)
+
+(defun init()
+	(setq logograms NIL)
+	(setq left-to-right NIL)
+	(setq abjad NIL)
+	(setq cyrulic NIL)
+	(setq accents NIL)
 )
 
-(defmacro lang(name logograms ltr abjad cyrulic acc)
-	(make-language :name name :logograms logograms :left-to-right ltr :abjad abjad :cyrulic cyrulic :accents acc)
+; define store variables\
+(defvar yes-logograms)
+(defvar yes-left-to-right)
+(defvar yes-abjad)
+(defvar yes-cyrulic)
+(defvar yes-accents)
+
+
+(defun clear-answers()
+	(setq yes-logograms NIL)
+	(setq yes-left-to-right NIL)
+	(setq yes-abjad NIL)
+	(setq yes-cyrulic NIL)
+	(setq yes-accents NIL)
 )
 
-(defmacro createSplit(attribute)
-	`(defun split(a splits)
-		(if (,attribute a)
-			(push a (first splits))
-			(push a (second splits))
+(defmacro make-attribute(attribute-name yes-var var question)
+	`(defun ,attribute-name()
+		(if ,var
+			,yes-var
+			(progn
+				(setq ,var T)
+				(if (query ,question)
+					(setq ,yes-var T)
+					(setq ,yes-var NIL)
+				)
+				,yes-var
+			)
 		)
-	splits
-	)
-)
-(defvar languages)
-
-(setq languages `(
-	,(lang "chinese" T NIL NIL NIL NIL)
-	,(lang "arabic" NIL T T NIL NIL)
-	,(lang "hebrew" NIL T NIL NIL NIL)
-	,(lang "russian" NIL NIL NIL T NIL)
-	,(lang "french" NIL NIL NIL NIL T)
-	,(lang "french" NIL NIL NIL NIL NIL)
 	)
 )
 
-;(reduce (createSplit eval) '(T T T NIL) :initial-value (list nil nil) :from-end T)
+; create attribute functions
+(make-attribute ask-logograms yes-logograms logograms "Does the language use logograms?")
+(make-attribute ask-left-to-right yes-left-to-right left-to-right "Is the language writter from left to right?")
+(make-attribute ask-abjad yes-abjad abjad "Does the language use the abjad?")
+(make-attribute ask-cyrulic yes-cyrulic cyrulic "Does the language use the cyrulic alphabet?")
+(make-attribute ask-accents yes-accents accents "Does the language contain accents?")
 
-(reduce (createSplit language-logograms) languages :initial-value (list nil nil) :from-end T)
+(defun chinese()
+	(ask-logograms)
+)
+
+(defun start
+	dolist
+)
+
+(defun query(question)
+	(y-or-n-p question)
+)
