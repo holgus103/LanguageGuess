4a0f07f2eb0a6fe04afa1205cd1a92665f9749d5
holgus103 <kubasuchan@hotmail.com>
2017-12-13 00:12:50 +0100

    Added macros, comments and intro

--- LanguageGuessLisp/main.lisp
+++ LanguageGuessLisp/main.lisp
@@ -1,39 +1,77 @@
-(defvar engine)
+; This is a simple Lisp reasoning expert system
+; In order to run the application call the start function as follows:
+; (start)
+; Then respond to the questions asked with either y or n
+; Example: 
+; Does the language use logograms? (y or n) n
+
+
 ; variables for storing information whether a question has been asked already
-(defvar logograms)
-(defvar left-to-right)
-(defvar abjad)
-(defvar cyrulic)
-(defvar accents)
+(defvar engine)
 
+; macro used for bulk variable definitions
+(defmacro bulk-define (vars)
+  `(progn
+     ,@(loop for var in vars
+             collect `(defvar ,var)
+		)
+	)
+)
+
+; macro used for bulk initialization
+(defmacro bulk-init (vars)
+  `(progn
+     ,@(loop for var in vars
+             collect `(setq ,var NIL)
+		)
+	)
+)
+
+; recursive macro used for bulk variable definitions
+(defmacro bulk-define-rec(vars)
+	(if (car vars)
+		`(progn
+			(defvar ,(car vars) NIL) 
+			(bulk-define-rec ,(cdr vars))
+		)
+	)
+)
+
+; recursive macro used for bulk initialization
+(defmacro bulk-init-rec(vars)
+	(if (car vars)
+		`(progn
+			(setq ,(car vars) NIL) 
+			(bulk-init ,(cdr vars))
+		)
+	)
+)
+
+; define query variables
+(bulk-define (logograms left-to-right abjad cyrulic accents))
+; intialize query variables
+(bulk-init (logograms left-to-right abjad cyrulic accents))
+; define answer variables
+(bulk-define-rec (yes-logograms yes-left-to-right yes-abjad yes-cyrulic yes-accents))
+; initialize answer variables
+(bulk-init-rec (yes-logograms yes-left-to-right yes-abjad yes-cyrulic yes-accents))
+
+; function clearing all query variables
 (defun init()
-	(setq logograms NIL)
-	(setq left-to-right NIL)
-	(setq abjad NIL)
-	(setq cyrulic NIL)
-	(setq accents NIL)
+	(bulk-init (logograms left-to-right abjad cyrulic accents))
 )
 
-; define store variables\
-(defvar yes-logograms)
-(defvar yes-left-to-right)
-(defvar yes-abjad)
-(defvar yes-cyrulic)
-(defvar yes-accents)
-
-
+; funciton clearing all answer variables
 (defun clear-answers()
-	(setq yes-logograms NIL)
-	(setq yes-left-to-right NIL)
-	(setq yes-abjad NIL)
-	(setq yes-cyrulic NIL)
-	(setq yes-accents NIL)
+	(bulk-init-rec (yes-logograms yes-left-to-right yes-abjad yes-cyrulic yes-accents))
 )
 
+; function querying user input
 (defun query(question)
 	(y-or-n-p question)
 )
 
+; function defining functions for various attribute lookups
 (defmacro make-attribute(attribute-name yes-var var question)
 	`(defun ,attribute-name()
 		(if ,var
@@ -57,32 +95,40 @@
 (make-attribute ask-cyrulic yes-cyrulic cyrulic "Does the language use the cyrulic alphabet?")
 (make-attribute ask-accents yes-accents accents "Does the language contain accents?")
 
+; chinese attributes definition
 (defun chinese()
 	(ask-logograms)
 )
 
+; arabic attributes definition
 (defun arabic()
 	(and (ask-left-to-right) (ask-abjad))
 )
 
+; hebrew attributes definition
 (defun hebrew()
 	(ask-left-to-right)
 )
 
+; russian attributes definition
 (defun russian()
 	(ask-cyrulic)
 )
 
+; french attributes definition
 (defun french()
 	(ask-accents)
 )
 
+; german attributes definition
 (defun german()
 	T
 )
 
+; initialize languages
 (setq engine '(("chinese" chinese)("arabic" arabic)("hebrew" hebrew)("russian" russian)("french" french)("german" german)))
 
+; main function
 (defun start()
 	(dolist(v engine)
 		(if (funcall (second v))
